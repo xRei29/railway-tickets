93e16892f7efc06ba33feec1dc9cc40940ccbe10
Roman Konopka <konopkaroman1@gmail.com>
2026-05-22 17:48:21 +0300

    feat: підключено BookingProvider до кореневого компонента в main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { BookingProvider } from './context/BookingContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* BookingProvider на найвищому рівні — доступ до контексту без prop drilling */}
+    <BookingProvider>
+      <App />
+    </BookingProvider>
   </React.StrictMode>
 )
