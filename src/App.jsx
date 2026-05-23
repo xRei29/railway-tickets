ed6c1767f4e73252121df4f4fdf49ad0edfaefe2
Roman Konopka <konopkaroman1@gmail.com>
2026-05-23 14:07:13 +0300

    feat: додано маршрут /booking/:trainId та підключено react-toastify в App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Home from './pages/Home'
+import Booking from './pages/Booking'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/booking/:trainId" element={<Booking />} />
       </Routes>
+
+      <ToastContainer
+        position="bottom-right"
+        autoClose={4000}
+        theme="dark"
+      />
     </BrowserRouter>
   )
 }
